8dd97d8758f4e2dbce11b8a8d5dc8885e1356e7a
Suhail Ansari <SuhailAnsari0201@outlook.com>
2020-04-27 17:21:17 +0530

    add chapter 9 part 7

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,13 @@ import Landing from "./comonents/layout/Landing";
 import Login from "./comonents/auth/Login";
 import Register from "./comonents/auth/Register";
 import Alert from "./comonents/layout/Alert";
+import Dashboard from "./comonents/dashboard/Dashboard";
+import CreateProfile from "./comonents/profile-forms/CreateProfile";
+import EditProfile from "./comonents/profile-forms/EditProfile";
+import AddExperience from "./comonents/profile-forms/AddExperience";
+import AddEducation from "./comonents/profile-forms/AddEducation";
+
+import PrivateRoute from "./comonents/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -12,6 +19,7 @@ import { loadUser } from "./_action/auth";
 import setAuthToken from "./_utils/setAuthToken";
 
 import "./App.css";
+import { addEducation } from "./_action/profile";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -32,6 +40,27 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
+              />
             </Switch>
           </section>
         </Fragment>
--- client/src/comonents/layout/Landing.js
+++ client/src/comonents/layout/Landing.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-const Landing = () => {
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
   return (
     <section className="landing">
       <div className="dark-overlay">
@@ -24,4 +29,13 @@ const Landing = () => {
     </section>
   );
 };
-export default Landing;
+
+Landing.prototype = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateTpProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateTpProps)(Landing);
